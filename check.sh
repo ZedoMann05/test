b37f4195dc3a999a0bb96b1953e150497de6ce3b
ZedoMann05 <161082715+ZedoMann05@users.noreply.github.com>
2024-02-27 19:12:43 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -15,6 +15,20 @@ declare -A limits=(
 folder_to_check="./public"
 IGNORED_ASSETS="./public/video/wellcome_ru.mp4"
 # Функція для перевірки розміру файлу та порівняння з лімітом для відповідного типу файлу
+convert() {
+    local bytes=$1
+    if (( bytes < 1024 )); then
+        echo "${bytes} bytes"
+    elif (( bytes < 1048576 )); then
+        echo "$(( bytes / 1024 )) KB"
+    elif (( bytes < 1073741824 )); then
+        printf "%.2f MB" "$(echo "scale=2; $bytes / 1024 / 1024" | bc)"
+    else
+        printf "%.2f GB" "$(echo "scale=2; $bytes / 1024 / 1024 / 1024" | bc)"
+    fi
+}
+
+
 check_file_size() {
     local file="$1"
     local size=$(stat -c %s "$file")
@@ -23,19 +37,6 @@ check_file_size() {
     local limit="${limits[$extension]}"
     
     # Функція для конвертації розміру з байтів у зручний формат
-    convert() {
-        local bytes=$1
-        if (( bytes < 1024 )); then
-            echo "${bytes} bytes"
-        elif (( bytes < 1048576 )); then
-            echo "$(( bytes / 1024 )) KB"
-        elif (( bytes < 1073741824 )); then
-            printf "%.2f MB" "$(echo "scale=2; $bytes / 1024 / 1024" | bc)"
-        else
-            printf "%.2f GB" "$(echo "scale=2; $bytes / 1024 / 1024 / 1024" | bc)"
-        fi
-    }
-
     if [ -n "$limit" ]; then
         if [ "$size" -gt "$limit" ]; then
             excess=$((size - limit))
@@ -44,14 +45,44 @@ check_file_size() {
     fi
 }
 
+check_ignor_size() {
+    local file="$1"
+    local size=$(stat -c %s "$file")
+    local extension="${file##*.}"  # Визначаємо розширення файлу
+    local extension="${extension,,}"  # Перетворюємо розширення в нижній регістр
+    local limit="${limits[$extension]}"
+    
+    # Функція для конвертації розміру з байтів у зручний формат
+    if [ -n "$limit" ]; then
+        if [ "$size" -gt "$limit" ]; then
+            excess=$((size - limit))
+            echo -e "Warning: File $file exceeds the limit for type .$extension \e[31mSize\e[0m: $(convert $size) (\e[32mLimit\e[0m: $(convert $limit))"
+        fi
+    fi
+}
+
 # Рекурсивна функція для обходу файлів у папках
 recursive_check() {
+    local current_folder="$1"
+    for file in "$current_folder"/*; do
+        # Перевіряємо, чи файл не є серед ігнорованих ассетів
+        if ! [[ " ${IGNORED_ASSETS[*]} " =~ " ${file} " ]]; then
+            if [ -f "$file" ]; then
+                check_file_size "$file"
+            elif [ -d "$file" ]; then
+                recursive_check "$file"
+            fi
+        fi
+    done
+}
+
+recursive_ignor() {
     local current_folder="$1"
     for file in "$current_folder"/*; do
         if [ -f "$file" ]; then
             check_file_size "$file"
         elif [ -d "$file" ]; then
-            recursive_check "$file"
+            recursive_ignor "$file"
         fi
     done
 }
@@ -61,12 +92,5 @@ recursive_check "$folder_to_check"
 # Перевірка проігнорованих ассетів
 ignored_assets_exceeding_limit=""
 for ignore_path in $IGNORED_ASSETS; do
-    output=$(recursive_check "$ignore_path")
-    if [ ! -z "$output" ]; then
-        ignored_assets_exceeding_limit="$ignored_assets_exceeding_limit$output\n"
-    fi
+    output=$(recursive_ignor "$ignore_path")
 done
-
-if [ ! -z "$ignored_assets_exceeding_limit" ]; then
-    echo -e "Ignored assets exceeding the limit:\n$ignored_assets_exceeding_limit"
-fi
