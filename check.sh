b202eec6d8991239d53947a5484af0a737209d8a
ZedoMann05 <161082715+ZedoMann05@users.noreply.github.com>
2024-02-27 18:25:45 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 declare -A limits=(
-    ["js"]=$js   # 1MB для .js файлів
-    ["txt"]=$txt   # 500KB для .txt файлів
-    ["svg"]=$svg  # 2MB для .svg файлів
-    ["png"]=$png  # 3MB для .png файлів
-    ["jpg"]=$jpg  # 3MB для .jpg файлів
-    ["json"]=$json # 1MB для .json файлів
-    ["css"]=$css  # 1MB для .css файлів
-    ["mp4"]=$mp4 # 20MB для .mp4 файлів
-    ["ico"]=$ico    # 50KB для .ico файлів
+    ["js"]=${{ vars.js }}  # 1MB для .js файлів
+    ["txt"]=${{ vars.txt }}   # 500KB для .txt файлів
+    ["svg"]=${{ vars.svg }}  # 2MB для .svg файлів
+    ["png"]=${{ vars.png }}  # 3MB для .png файлів
+    ["jpg"]=${{ vars.jpg }}  # 3MB для .jpg файлів
+    ["json"]=${{ vars.json }} # 1MB для .json файлів
+    ["css"]=${{ vars.css }}  # 1MB для .css файлів
+    ["mp4"]=${{ vars.mp4 }} # 20MB для .mp4 файлів
+    ["ico"]=${{ vars.ico }}    # 50KB для .ico файлів
 )
 
 folder_to_check="./public"
