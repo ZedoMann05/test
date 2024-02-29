09885feea6c58dc5694d819d9ac3bd301d5e98c1
Bohdan Havran <danohavran@gmail.com>
2024-02-29 18:42:54 +0200

    test

--- check.sh
+++ check.sh
@@ -78,6 +78,9 @@ recursive_check "$asset_paths"
 # Print report to ./report.txt
 echo "Assets Size Validation Report" > ./report.txt
 
+# Print report
+echo "Assets Size Validation Report" > ./report.txt
+
 if [ ${#errors[@]} -gt 0 ]; then
     echo "Status: FAILED" >> ./report.txt
     echo -e "\nSome assets exceed the specified limit in the following directories: $asset_paths." >> ./report.txt
@@ -89,18 +92,26 @@ if [ ${#errors[@]} -gt 0 ]; then
         echo -e "Limit: $(convert ${limits[$extension]})" >> ./report.txt
         echo -e "${errors[$extension]}" >> ./report.txt
     done
-elif [ ${#warnings[@]} -gt 0 ]; then
-    echo "Status: WARNING" >> ./report.txt
-    echo -e "\nSome assets exceed the specified limit in the following directories: $asset_paths, but they do not fail the validation because they are ignored by configuration." >> ./report.txt
+fi
+
+if [ ${#warnings[@]} -gt 0 ]; then
+    if [ ${#errors[@]} -gt 0 ]; then
+        echo -e "\nWarnings" >> ./report.txt
+    else
+        echo "Status: WARNING" >> ./report.txt
+        echo -e "\nSome assets exceed the specified limit in the following directories: $asset_paths, but they do not fail the validation because they are ignored by configuration." >> ./report.txt
+    fi
+
     echo -e "\nTotal Warnings: ${#warnings[@]}." >> ./report.txt
-    echo -e "\nWarnings" >> ./report.txt
 
     for extension in "${!warnings[@]}"; do
         echo -e "$extension" >> ./report.txt
         echo -e "Limit: $(convert ${limits[$extension]})" >> ./report.txt
         echo -e "${warnings[$extension]}" >> ./report.txt
     done
-else
+fi
+
+if [ ${#errors[@]} -eq 0 ] && [ ${#warnings[@]} -eq 0 ]; then
     echo "Status: SUCCESS" >> ./report.txt
     echo -e "\nAll assets match the size limit for their file types in the following directories: $asset_paths." >> ./report.txt
     echo -e "\nNo actions required." >> ./report.txt
